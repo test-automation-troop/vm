fdf4be2b2713b156f1fe91a20ba622ec2cf349e0
cliz-acc <chaitanya.liz.r.t@accenture.com>
2026-09-15 12:26:57 +0530

    update backend

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name = "drift-test"
-        storage_account_name = "testsaaccount5603"
-        container_name = "tfstate"
-        key = "vm/terraform.tfstate"
-    }
-}
